8e08a2c32bacae4b9edbe2d298b1e681fa180afe
Tibor Claassen <tibor@gmah.net>
2015-05-28 17:44:17 +0200

    Normalize names and ids for matching

--- test/aggregate.js
+++ test/aggregate.js
@@ -16,3 +16,39 @@ it('aggregates cases with matching id and name', function (expect) {
   expect.lengthOf(result, 2)
   expect.end()
 })
+
+it('ignores case when matching', function (expect) {
+  var aggregator = Object.create(null)
+  var doc = fakeDoc({ contact: { sourceCases:
+    [ { id: 'ID-0203', name: 'JOHN DOE' }
+    , { id: 'Id-0203', name: 'John Doe' }
+    ]
+  }})
+  var result = migrate(doc, aggregator)
+  expect.lengthOf(result, 2)
+  expect.end()
+})
+
+it('ignores whitespace differences when matching', function (expect) {
+  var aggregator = Object.create(null)
+  var doc = fakeDoc({ contact: { sourceCases:
+    [ { id: 'ID-0203', name: 'JOHN   DOE' }
+    , { id: ' ID-0203 ', name: 'JOHN DOE' }
+    ]
+  }})
+  var result = migrate(doc, aggregator)
+  expect.lengthOf(result, 2)
+  expect.end()
+})
+
+it('ignores abbreviation dots when matching', function (expect) {
+  var aggregator = Object.create(null)
+  var doc = fakeDoc({ contact: { sourceCases:
+    [ { id: 'ID-0203', name: 'JOHN P. DOE' }
+    , { id: 'ID-0203', name: 'JOHN P DOE' }
+    ]
+  }})
+  var result = migrate(doc, aggregator)
+  expect.lengthOf(result, 2)
+  expect.end()
+})
